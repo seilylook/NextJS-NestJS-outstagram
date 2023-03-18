7fe6ed759cd7bf5641f40202a534460d70b34f96
seilylook <seilylook@naver.com>
2023-03-18 13:21:14 +0900

    FIX: without login ban to go profile page

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootReducerState } from "@/reducers";
 import Head from "next/head";
+import Router from "next/router";
 
 import AppLayout from "@/components/AppLayout";
 import NicknameEditForm from "@/components/NicknameEditForm";
@@ -8,6 +10,16 @@ import FollowList from "@/components/FollowList";
 
 const Profile = () => {
   const { me } = useSelector((state: RootReducerState) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
