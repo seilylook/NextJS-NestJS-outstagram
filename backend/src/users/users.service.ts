dcaa467e8efd534c30f538e67d167c9410a86b0d
seilylook <seilylook@naver.com>
2023-03-21 12:16:49 +0900

    FEAT: user signup with hashedpassword implement

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,20 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Repository, DataSource } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private userRepository: Repository<Users>,
+    private dataSource: DataSource,
   ) {}
 
   async signUp(email: string, nickname: string, password: string) {
-    await this.userRepository.save({
-      email,
-      nickname,
-      password,
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const user = await this.userRepository.findOne({
+      where: { email },
     });
+
+    if (user) {
+      throw new Error('이미 존재하는 사용자입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    try {
+      const result = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
