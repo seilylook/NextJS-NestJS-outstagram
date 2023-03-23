25f095a29cff72c337bec3e931850c8f6ef3ed4d
seilylook <seilylook@naver.com>
2023-03-23 12:17:45 +0900

    FEAT: add mainPosts data to check state

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -15,14 +15,45 @@ export class PostsController {
   constructor(private postsService: PostsService) {}
 
   // --- mainPosts---
-  // id: data.id,
-  // content: data.content,
-  // User: {
-  //   id: 1,
-  //   nickname: "kim",
-  // },
-  // Images: [],
-  // Comments: [],
+  // mainPosts: [
+  //   {
+  //     id: 1,
+  //     User: {
+  //       id: 1,
+  //       nickname: "kim",
+  //     },
+  //     content: "첫 번째 게시글. #Next #Nest",
+  //     Images: [
+  //       {
+  //         src: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+  //       },
+  //       {
+  //         src: "https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg",
+  //       },
+  //       {
+  //         src: "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg",
+  //       },
+  //     ],
+  //     Comments: [
+  //       {
+  //         id: 1,
+  //         User: {
+  //           id: 2,
+  //           nickname: "lee",
+  //         },
+  //         content: "첫 댓글이다",
+  //       },
+  //       {
+  //         id: 2,
+  //         User: {
+  //           id: 3,
+  //           nickname: "park",
+  //         },
+  //         content: "두번째 댓글이다.",
+  //       },
+  //     ],
+  //   },
+  // ],
 
   @Get('/')
   async loadAllPosts() {
