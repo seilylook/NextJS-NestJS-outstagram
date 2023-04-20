6fde90ea198b9ec47f27f152cd0fc2770525a7d6
seilylook <seilylook@naver.com>
2023-04-20 21:09:25 +0900

    FEAT: docker images upload in kubernetes cluster

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = {
+//   nextConfig,
+// };
 
 module.exports = {
-  nextConfig,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    const plugins = [...config.plugins];
+
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins,
+    };
+  },
 };
